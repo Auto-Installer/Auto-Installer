3512d94e31b8934138baece155655cc74c901bfa
nabilkhalil <duomaster01@gmail.com>
2018-07-16 22:10:44 -0700

    routing from the login page to the home page has been set up

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import login.loginController;
+import home.homeController;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("./login/login.fxml"));
+
+        // Sets Login Stage
+        FXMLLoader loginPaneLoader = new FXMLLoader(getClass().getResource("./login/login.fxml"));
+        Parent loginPane = loginPaneLoader.load();
+        Scene loginScene = new Scene(loginPane, 1280, 800);
+
+        // Sets Home Stage
+        FXMLLoader homePaneLoader = new FXMLLoader(getClass().getResource("./home/home.fxml"));
+        Parent homePane = homePaneLoader.load();
+        Scene homeScene = new Scene(homePane, 1280, 800);
+
+        // injecting home scene into the controller of the login scene
+        loginController loginPaneController = (loginController) loginPaneLoader.getController();
+        loginPaneController.setHomeScene(homeScene);
+
+
         primaryStage.setTitle("Auto-Installer");
-        primaryStage.setScene(new Scene(root, 1280, 800));
+        primaryStage.setScene(loginScene);
         primaryStage.show();
     }
 
--- src/login/loginController.java
+++ src/login/loginController.java
@@ -1,20 +1,36 @@
 package login;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 import java.awt.*;
 
+// Handles logins
 
 public class loginController {
-    @FXML Button submitLogin;
     @FXML TextField usernameInput;
     @FXML PasswordField passwordInput;
 
-    public void submitLogin(){
-        System.out.println(usernameInput.getText());
-        System.out.println(passwordInput.getText());
+    private Scene homeScene;
+
+    public void setHomeScene(Scene scene) {
+        homeScene = scene;
+    }
+
+    public void submitLogin(ActionEvent actionEvent){
+
+        if((usernameInput.getText().equals("john")) && (passwordInput.getText().equals("doe"))) {
+            System.out.println("Successfully logged in!");
+            Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            primaryStage.setScene(homeScene);
+        }
+        else{
+            System.out.println("Those credentials were wrong");
+        }
 
     }
 
