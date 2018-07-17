d201443c4070385d13cbde2825b9b99a30695e38
nabilkhalil <duomaster01@gmail.com>
2018-07-16 19:17:32 -0700

    able to get username and password input and log it

--- src/login/loginController.java
+++ src/login/loginController.java
@@ -1,4 +1,21 @@
 package login;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 
 public class loginController {
+    @FXML Button submitLogin;
+    @FXML TextField usernameInput;
+    @FXML PasswordField passwordInput;
+
+    public void submitLogin(){
+        System.out.println(usernameInput.getText());
+        System.out.println(passwordInput.getText());
+
+    }
+
 }
